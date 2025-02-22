4178d507b9e7124a395c684efab8bcbb1cffba49
hyde-mary <hydemaryalzbayne@gmail.com>
2025-02-22 18:31:49 +0800

    feat: update naming convention to industry standards

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,8 @@ import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
   "/landing",
-  "/login(.*)",
-  "/register(.*)",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
