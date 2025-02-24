74b9532e42844e8c80890d06c7ecbf4ae9251f03
hyde-mary <hydemaryalzbayne@gmail.com>
2025-02-24 21:11:01 +0800

    feat: Add Link to go back to root

--- app/_Components/navbar.tsx
+++ app/_Components/navbar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { useUser } from "@clerk/nextjs";
 import NavbarActions from "./navbar-actions";
+import Link from "next/link";
 
 const Navbar = () => {
   const { user } = useUser();
@@ -17,15 +18,20 @@ const Navbar = () => {
       className="fixed top-0 left-0 w-full z-50 h-16 flex items-center justify-between px-4 sm:px-6 border-b border-gray-300 bg-white shadow-md"
     >
       {/* Logo */}
+
       <motion.div
         whileHover={{ scale: 1.05 }}
         transition={{ type: "spring", stiffness: 300 }}
         className="flex space-x-2 text-xl sm:text-2xl"
       >
-        <p>✨</p>
-        <h1 className="font-bold bg-gradient-to-r from-purple-600 to-blue-600 bg-clip-text text-transparent">
-          Missed Connections
-        </h1>
+        <Link href="/">
+          <div className="flex">
+            <p>✨</p>
+            <h1 className="font-bold bg-gradient-to-r from-purple-600 to-blue-600 bg-clip-text text-transparent">
+              Missed Connections
+            </h1>
+          </div>
+        </Link>
       </motion.div>
 
       <NavbarActions />
